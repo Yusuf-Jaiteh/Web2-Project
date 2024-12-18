2b7d0d4dc83fb4d9fa1423bcb44d380fc122c6bf
Yusuf-Jaite <yusufjaite77@gmail.com>
2024-12-17 19:01:48 -0800

    I added login and sign up components

---
+++ learning_management_system/src/app/app.routes.ts
@@ -0,0 +1,8 @@
+import { Routes } from '@angular/router';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { SignInComponent } from './components/sign-in/sign-in.component';
+
+export const routes: Routes = [
+    {path: '/sign-up', component: SignUpComponent},
+    {path: '/sign-in', component: SignInComponent}
+];
